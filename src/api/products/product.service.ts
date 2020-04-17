39bada4d1c1d6b26523a93eeed92288875ce784c
waikon.tse <waikon.tse@teamrockstars.nl>
2020-04-17 12:22:41 +0200

    Add error http client with error handling

--- src/api/products/product.service.ts
+++ src/api/products/product.service.ts
@@ -1,7 +1,8 @@
 import { Product } from './product';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'
+import { Observable, throwError } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
 
 
 @Injectable()
@@ -10,8 +11,24 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) {
   }
-
+ 
   public getProducts(): Observable<Product[]> {
-    return this.httpClient.get<Product[]>(this.productUrl);
+    return this.httpClient.get<Product[]>(this.productUrl).pipe(
+      tap(data => console.log('All: ' + JSON.stringify(data))), 
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    let errorMsg = '';
+    
+    if (err.error instanceof ErrorEvent) {
+      errorMsg = `An error occured: ${err.error.message}`;
+    } else {
+      errorMsg = `Server returned code ${err.status}, error message is ${err.message}`;
+    }
+
+    console.log(errorMsg);
+    return throwError(errorMsg);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
-import { HttpModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +16,7 @@ import { ProductManagerModule } from './product-manager/product-manager.module';
     AppRoutingModule,
     ProductManagerModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
